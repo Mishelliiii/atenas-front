7f342652525be98a8bd4599b7edf0b750e8c747d
Aluno41 <aluno@dinalva41.academico.cotuca.unicamp.br>
2020-07-19 20:08:22 -0300

    Menu e ligação de paginas

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,19 @@ import { Switch, Route } from 'react-router-dom';
 
 import Home from '../pages/Home';
 import Login from '../pages/Login';
+import Adm from '../pages/Adm/adm';
+import MinhaConta from '../pages/MinhaConta/minhaConta';
+
+
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/login" component={Login} />
+      <Route path="/Adm" component={Adm} />
+      <Route path="/MinhaConta" component={MinhaConta} />
+
     </Switch>
   );
 }
